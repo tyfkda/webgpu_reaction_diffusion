a58848caf6852380da4ea5aa7c625b63c966fed5
tyfkda <tyfkda@gmail.com>
2023-09-20 11:11:01 +0900

    マウスで消せるようにする

--- main.js
+++ main.js
@@ -156,20 +156,28 @@ function irandom(min, max) {
 
 class MyApp extends WgslFramework {
   page = 0
+  erasePos = []
+  drawing = false
 
   setUpComputeData() {
     const BUFFER_SIZE = GRID_SIZE * GRID_SIZE * 2 * 4
+
+    this.stagingBuffer = this.device.createBuffer({
+      label: 'Staging buffer',
+      size: BUFFER_SIZE,
+      usage: GPUBufferUsage.MAP_READ | GPUBufferUsage.COPY_DST,
+    })
 
     const cellStateStorage = [
       this.device.createBuffer({
         label: 'Cell State A',
         size: BUFFER_SIZE,
-        usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+        usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST | GPUBufferUsage.COPY_SRC,
       }),
       this.device.createBuffer({
         label: 'Cell State B',
         size: BUFFER_SIZE,
-        usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST,
+        usage: GPUBufferUsage.STORAGE | GPUBufferUsage.COPY_DST | GPUBufferUsage.COPY_SRC,
       }),
     ]
 
@@ -361,6 +369,10 @@ class MyApp extends WgslFramework {
   }
 
   draw() {
+    if (this.drawing)
+      return
+    this.drawing = true
+
     const encoder = this.device.createCommandEncoder()
 
     for (let i = 0; i < 8; ++i) {
@@ -389,7 +401,68 @@ class MyApp extends WgslFramework {
     pass.draw(this.vertices.length / 2, GRID_SIZE * GRID_SIZE)
     pass.end()
 
+    if (this.erasePos.length > 0) {
+      const output = this.cellStateStorage[this.page]
+      encoder.copyBufferToBuffer(
+        output,
+        0, // Source offset
+        this.stagingBuffer,
+        0, // Destination offset
+        this.stagingBuffer.size,
+      )
+    }
+
     this.device.queue.submit([encoder.finish()])
+
+    if (this.erasePos.length <= 0) {
+      this.drawing = false
+    } else {
+      this.runErase()
+        .then(() => {
+          this.drawing = false
+        })
+    }
+  }
+
+  async runErase() {
+    const BUFFER_SIZE = this.stagingBuffer.size
+    this.stagingBuffer.mapAsync(
+      GPUMapMode.READ,
+      0, // Offset
+      BUFFER_SIZE // Length
+    ).then(() => {
+      const copyArrayBuffer = this.stagingBuffer.getMappedRange(0, BUFFER_SIZE)
+      const data = copyArrayBuffer.slice()
+      this.stagingBuffer.unmap()
+      const cellStateArray = new Float32Array(data)
+
+      const width = GRID_SIZE
+      const height = GRID_SIZE
+
+      for (let i = 0; i < this.erasePos.length; i += 2) {
+        const cx = this.erasePos[i]
+        const cy = this.erasePos[i + 1]
+
+        const radius = (GRID_SIZE / 16) | 0, radius2 = radius * radius
+        const x = cx | 0, y = cy | 0
+        const dx0 = Math.max(-radius, -x), dy0 = Math.max(-radius, -y)
+        const dx1 = Math.min(radius, width - 1 - x), dy1 = Math.min(radius, height - 1 - y)
+        for (let dy = dy0; dy <= dy1; ++dy) {
+          for (let dx = dx0; dx <= dx1; ++dx) {
+            if (dx * dx + dy * dy >= radius2)
+              continue
+            cellStateArray[((y + dy) * GRID_SIZE + (x + dx)) * 2 + 1] = 0  // b = 0
+          }
+        }
+      }
+      this.erasePos.length = 0
+
+      this.device.queue.writeBuffer(this.cellStateStorage[this.page], 0, cellStateArray)
+    })
+  }
+
+  async pushErase(cx, cy) {
+    this.erasePos.push(cx, cy)
   }
 }
 
@@ -406,6 +479,30 @@ async function main() {
 
   document.getElementById('reset-btn').addEventListener('click', () => {
     myapp.randomizeCellState()
+  })
+
+  const canvas = document.querySelector('canvas')
+  canvas.addEventListener('mousedown', (event) => {
+    const clientRect = canvas.getBoundingClientRect()
+    const erase = (ev) => {
+      const cx = ev.clientX - clientRect.x
+      const cy = ev.clientY - clientRect.y
+      // console.log(`${cx}, ${cy}`)
+      if (0 <= cx && cx < canvas.width && 0 <= cy && cy < canvas.height) {
+        myapp.pushErase(cx * GRID_SIZE / clientRect.width, (clientRect.height - 1 - cy) * GRID_SIZE / clientRect.height)
+      }
+    }
+    erase(event)
+
+    const mousemove = (event) => {
+      erase(event)
+    }
+    const mouseup = (_event) => {
+      document.removeEventListener('mousemove', mousemove)
+      document.removeEventListener('mouseup', mouseup)
+    }
+    document.addEventListener('mousemove', mousemove)
+    document.addEventListener('mouseup', mouseup)
   })
 }
 
